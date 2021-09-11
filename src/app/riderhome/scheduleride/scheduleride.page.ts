fd32fa76ff237750ab673ad5de02beb8aa588847
ShreyasAmbre <shreyas@rupeeseed.com>
2021-09-11 09:25:13 +0530

    Schedule Ride Changes and Popup form created

--- src/app/riderhome/scheduleride/scheduleride.page.ts
+++ src/app/riderhome/scheduleride/scheduleride.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-scheduleride',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduleride.page.scss'],
 })
 export class ScheduleridePage implements OnInit {
+  source:any = "";
+  destination:any = "";
 
-  constructor() { }
+  constructor(public alertController: AlertController, public router:Router) { }
+
+  async scheduleride() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Schedule Ride',
+      inputs: [
+        // input date without min nor max
+        {
+          name: 'Date',
+          type: 'date',
+        },
+        {
+          name: 'Time',
+          type: 'time',
+        },
+        {
+          name: 'Cost',
+          type: 'number',
+          placeholder: "Cost"
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Submit',
+          handler: (inputs) => {
+            this.bookride(inputs)
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
   ngOnInit() {
+  }
+
+  bookride(scheduledata){
+    console.log("BOOK RIDE ===>", scheduledata, this.source, this.destination)
+    this.router.navigate(['/riderhome/riderhome/upcomingrides']);
   }
 
 }
